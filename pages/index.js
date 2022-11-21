bdedf914d68b3e9a0c708e52305427732914a6b8
smmmfrd <smmmfrd@gmail.com>
2022-11-21 14:04:45 -0800

    Tailwind Install

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>My Portfolio Site!</h1>
+      <h1 className="text-3xl font-bold underline">My Portfolio Site!</h1>
     </div>
   )
 }
