e19f3d9df1905ba9749a65d6e3ba64da3d104c8a
smmmfrd <smmmfrd@gmail.com>
2022-12-07 17:22:33 -0800

    To Top and Nav Home Page Link Buttons

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,32 @@
+import { useEffect, useRef } from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 export default function Layout({ children }) {
+
+    const returnToTopButton = useRef();
+
+    useEffect(() => {
+        window.onscroll = function () { handleScroll() };
+    }, []);
+
+    function handleScroll() {
+        if(document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+            returnToTopButton.current.style.display = 'block';
+        } else {
+            returnToTopButton.current.style.display = 'none';
+        }
+    }
+
+    function handleTop() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+
     return (
         <div className="relative min-h-screen text-neutral-50
         bg-gradient-to-b from-cyan-500 to-blue-500">
+            <button ref={returnToTopButton} onClick={handleTop} className="pt-1 p-2 bg-black border-2 border-slate-400 rounded fixed bottom-5 right-5 z-20">Back to Top <span className="text-xl">&uarr;</span></button>
             <Navbar />
             {children}
             <Footer />
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,19 @@
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 export default function Navbar() {
     const router = useRouter();
 
-    useEffect(() => {
-        console.log(router.pathname);
-    }, [])
+    function handleClick() {
+        router.push('/');
+    }
 
     return (
         <header className="z-20 mb-2 border-b-4 p-3 sticky top-0 bg-cyan-500">
-            <nav className="max-w-5xl mx-auto">
+            <nav className="max-w-5xl mx-auto
+                flex justify-between">
                 <h1 className="text-2xl">Sam Mumford's Portfolio</h1>
+                {router.pathname !== '/' && <button onClick={handleClick}>Return to Home</button>}
             </nav>
         </header>
     );
