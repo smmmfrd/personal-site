8a9af12f654d83659a75004b281c2f040c36a0f9
smmmfrd <smmmfrd@gmail.com>
2023-01-09 13:34:43 -0800

    Dynamic Main Page Projects

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 
 import { db } from "../firebase";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 import Image from "next/image";
 
 import SocialLinks from "../components/SocialLinks";
@@ -14,78 +14,56 @@ import personalSitePic from '../public/project_pics/personal_site_projects.png';
 import wheresWaldoPic from '../public/project_pics/wheres_waldo_char-screen.png';
 import ContactMe from "../components/ContactMe";
 
-export async function getStaticProps() {
-  const colRef = collection(db, "projects");
-  const snapshot = await getDocs(colRef);
+// async function getProjects() {
+//   const projectColRef = collection(db, "projects");
+//   const projectSnapshot = await getDocs(projectColRef);
 
-  let docs = [];
-  snapshot.docs.forEach(doc => {
-    let data = doc.data();
-    docs.push({
-      ...data.header,
-      title: data.title,
-      id: doc.id
-    })
-  });
-  console.log(docs);
-  
+//   let docs = [];
+//   projectSnapshot.docs.forEach(doc => {
+//     let data = doc.data();
+//     docs.push({
+//       ...data.header,
+//       title: data.title,
+//       id: doc.id
+//     })
+//   });
+
+//   return docs;
+// }
+
+export async function getStaticProps() {
+  const mainPageDocRef = doc(db, "main page", "main");
+  const mainPageDocSnapshot = await getDoc(mainPageDocRef);
+
+  let mainPage = mainPageDocSnapshot.data();
+  console.log(mainPage)
   return {
-    props: { projects: docs }
+    props: {  mainPage }
   }
 }
 
-export default function Home({ projects }) {
+export default function Home({ mainPage }) {
   useEffect(() => {
-    console.log(projects);
+    console.log(mainPage);
   }, []);
-  
+
   return (
     <main className="lg:max-w-5xl sm:w-11/12 w-full mx-auto my-4 flex flex-col gap-8">
       <AboutMe />
       <section>
-        <h2 className="bg-blurredBg w-min pt-4 px-4 text-6xl rounded-t-xl z-10">Projects</h2>
+        <h2 className="bg-blurredBg w-min pt-4 px-4 text-6xl rounded-t-xl z-10">Featured Projects</h2>
         <div className="bg-blurredBg pt-0 sm:p-4 p-1 rounded-xl rounded-tl-none
             grid lg:grid-cols-2 grid-cols-1">
-          <ProjectCard
-            projectName={"Rookie Drive"}
-            projectDescription={"A clone of Google Drive, but only with document types typically made by people learning how to code. Users can sign in and create, update, or delete their own documents, all handled through Firebase."}
-            technologies={["react", "firebase", "webpack"]}
-            href={'rookie-drive'}
-            githubLink={"https://github.com/smmmfrd/rookie-drive"}
-            siteLink={"https://aesthetic-crisp-943a41.netlify.app/"}
-            img={rookieDrivePic}
-            altText={'The landing page of my Rookie Drive project, with example documents.'}
-          />
-          <ProjectCard
-            projectName={"Battleship"}
-            projectDescription={"Seeing React's overall architecture reflected exactly how I like to make games, so making a game in React happened naturally."}
-            technologies={["react", "tailwind", "react-router", "vite"]}
-            href={'battleship'}
-            githubLink={"https://github.com/smmmfrd/battleship-react"}
-            siteLink={"https://velvety-moxie-6a1467.netlify.app/"}
-            img={battleShipPic}
-            altText={'The end screen of a game of Battleship in my project.'}
-          />
-          <ProjectCard
-            projectName={"Personal Site"}
-            projectDescription={"This site itself, made in Next.js and published through Netlify."}
-            technologies={["react", "tailwind", "next"]}
-            href={'personal-site'}
-            githubLink={"https://github.com/smmmfrd/personal-site"}
-            siteLink={"#"}
-            img={personalSitePic}
-            altText={'The Projects section of my personal site.'}
-          />
-          <ProjectCard
-            projectName={"Where's Waldo App"}
-            projectDescription={"A Where's Waldo clone, but functionally is a quiz app without the correct answers stored on the client, done through checking attempts to a Firebase hosted database."}
-            technologies={["react", "firebase", "webpack"]}
-            href={'wheres-waldo'}
-            githubLink={"https://github.com/smmmfrd/wheres-waldo-app"}
-            siteLink={"https://willowy-cascaron-acaae3.netlify.app/"}
-            img={wheresWaldoPic}
-            altText={'The beginning screen of a game of Where\'s Waldo in my project'}
-          />
+          {mainPage.featuredProjects.map((project) => <ProjectCard
+            projectName={project.title}
+            projectDescription={project.intro}
+            technologies={project.techUsed}
+            href={`${project.id}`}
+            githubLink={project.githubLink}
+            siteLink={project.liveLink}
+            img={project.image}
+            altText={''}
+          />)}
         </div>
       </section>
       <ExtraStuffs />
